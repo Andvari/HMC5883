674733194078fdcac3bc762d60fc5f3052198aef
Nemo <dedmazai74@mail.ru>
2012-09-26 15:07:02 +0400

    First Commit

---
+++ hmc5883.cpp
@@ -0,0 +1,10 @@
+/*
+ * hmc5883.cpp
+ *
+ *  Created on: Sep 26, 2012
+ *      Author: nemo
+ */
+
+
+
+
---
+++ hmc5883.h
@@ -0,0 +1,14 @@
+/*
+ * hmc5883.h
+ *
+ *  Created on: Sep 26, 2012
+ *      Author: nemo
+ */
+
+#ifndef HMC5883_H_
+#define HMC5883_H_
+
+
+
+
+#endif /* HMC5883_H_ */
